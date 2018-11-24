11a6d6c126d86d080a833f5ed782fc354125cd26
Corey McKinnon <cmckinnon@gmail.com>
2018-11-24 14:59:14 -0700

    Added all comics in, and applied some very basic styles

--- Models/ComicDefinition.cs
+++ Models/ComicDefinition.cs
@@ -5,5 +5,6 @@
         public string FullName { get; set; }
         public string Name { get; set; }
         public string Extension { get; set; }
+        public int SortOrder { get; set; }
     }
 }
